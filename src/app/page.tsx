c40b8c9ad9161e19149506812fabe87264dbec0b
Ben Walderman <benlwalderman@gmail.com>
2025-07-16 21:25:54 -0700

    replaced alert in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { socket } from "@/lib/socketClient";
 import { useRouter } from "next/navigation";
 
@@ -32,6 +32,9 @@ import { useBackground } from "@/utils/useBackground";
 import { Card, CardContent, CardTitle } from "@/components/ui/card";
 import { clickSound } from "@/utils/sounds";
 import PageTheme from "@/components/PageTheme";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertCircleIcon, Loader2Icon } from "lucide-react";
+import { Separator } from "@/components/ui/separator";
 
 const createLobbySchema = z.object({
   name: z.string().max(15, {
@@ -50,6 +53,10 @@ export default function Home() {
   const background = useBackground();
   const router = useRouter();
 
+  const [showAlert, setShowAlert] = useState(false);
+  const [joining, setJoining] = useState(false);
+  const [creating, setCreating] = useState(false);
+
   const form1 = useForm<z.infer<typeof createLobbySchema>>({
     resolver: zodResolver(createLobbySchema),
     defaultValues: {
@@ -66,19 +73,24 @@ export default function Home() {
   });
 
   const handleCreateLobby = (data: z.infer<typeof createLobbySchema>) => {
+    setCreating(true);
     localStorage.setItem("playerName", data.name);
     router.push("/create");
+    setCreating(false);
   };
 
   const handleJoinLobby = (data: z.infer<typeof joinLobbySchema>) => {
     const name = form1.getValues("name");
+    setJoining(true);
     socket.emit("checkLobby", data.lobbyId, (exists: boolean) => {
       if (!exists) {
-        alert("Error: Lobby full or does not exist");
+        setJoining(false);
+        setShowAlert(true);
         return;
       }
       localStorage.setItem("playerName", name);
       router.push(`/lobby/${data.lobbyId}`);
+      setJoining(false);
     });
   };
 
@@ -105,12 +117,12 @@ export default function Home() {
         <TypographyH1 className="mt-20 text-7xl font-sans">
           Werewolf
         </TypographyH1>
-        <Card className="bg-card/50 backdrop-blur-sm size-fit p-10 mx-auto my-30 flex flex-col items-center justify-center">
+        <Card className="size-fit p-10 mx-auto my-30 flex flex-col items-center justify-center bg-card/50 backdrop-blur-sm">
           <CardTitle className="text-xl">Enter a name to play!</CardTitle>
-          <CardContent className="flex flex-col items-center justify-center gap-4">
+          <CardContent className="flex flex-col items-center justify-center gap-4 bg-0">
             <Form {...form1}>
               <form
-                className="flex flex-col items-center justify-center gap-4"
+                className="flex flex-col items-center justify-center gap-4 bg-0"
                 onSubmit={form1.handleSubmit(handleCreateLobby)}
               >
                 <FormField
@@ -133,9 +145,9 @@ export default function Home() {
                   onClick={clickSound}
                   className="w-[200px]"
                   type="submit"
-                  disabled={!form1.watch("name")}
+                  disabled={!form1.watch("name") || creating}
                 >
-                  Create Lobby
+                  {creating ? <Loader2Icon className="animate-spin" /> : "Create Lobby"}
                 </Button>
               </form>
             </Form>
@@ -143,7 +155,10 @@ export default function Home() {
             <Dialog>
               <DialogTrigger asChild>
                 <Button
-                  onClick={clickSound}
+                  onClick={() => {
+                    clickSound();
+                    setShowAlert(false);
+                  }}
                   className="w-[200px]"
                   type="button"
                   disabled={!form1.watch("name")}
@@ -194,12 +209,30 @@ export default function Home() {
                         onClick={clickSound}
                         className="w-[100px]"
                         type="submit"
+                        disabled={joining}
                       >
-                        Join
+                        {joining ? <Loader2Icon className="animate-spin" /> : "Join"}
                       </Button>
                     </DialogFooter>
                   </form>
                 </Form>
+                {showAlert && (
+                  <>
+                    <Separator />
+                    <Alert variant="destructive">
+                      <AlertCircleIcon />
+                      <AlertTitle>Error joining lobby</AlertTitle>
+                      <AlertDescription>
+                        <p>Make sure that:</p>
+                        <ul className="list-inside list-disc text-sm">
+                          <li>The lobby ID is correct</li>
+                          <li>The lobby is not full</li>
+                          <li>The game has not started yet</li>
+                        </ul>
+                      </AlertDescription>
+                    </Alert>
+                  </>
+                )}
               </DialogContent>
             </Dialog>
           </CardContent>
