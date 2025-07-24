4d922e848668fa96e83e9ba94de173d9dcd7d34d
Ben Walderman <benlwalderman@gmail.com>
2025-07-24 08:23:04 -0700

    timer UI

--- src/app/game/[id]/CountdownTimer.tsx
+++ src/app/game/[id]/CountdownTimer.tsx
@@ -7,9 +7,9 @@ interface CountdownTimerProps {
 
 const CountdownTimer = ({ countdown }: CountdownTimerProps) => {
   return (
-    <div className="text-4xl font-mono font-bold flex">
+    <div className="text-4xl font-bold flex justify-start w-[4ch] items-center">
       <IoIosTimer />
-      <span className="w-[2ch] tabular-nums text-center">
+      <span className="w-fit text-center">
         {countdown ? countdown : ""}
       </span>
     </div>
